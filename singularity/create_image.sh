79e2076b1fb78db4430c062cf03c04fae19bd56b
Nidzo454 <nikolapetrovski@protonmail.com>
2025-09-19 13:12:53 +0200

    Singularity image creation and configuration files

---
+++ singularity/create_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=build_singularity
+#SBATCH --output=outputs/build_singularity.out
+#SBATCH --error=errors/build_singularity.err
+#SBATCH --cpus-per-task=4
+#SBATCH --nodelist=gpgpu01
+
+# Load Singularity
+module load singularity
+
+# Build the image
+singularity build ml4science.sif ml4science.def
